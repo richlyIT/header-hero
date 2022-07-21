3a9639bdbb5fd112b3ff75225dd0dd9031c65e8f
Fructuoso-Nguema <fabesongui@gmail.com>
2022-07-21 10:21:15 +0000

    fructuoso second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    //scrollTop
+
     $(window).scroll(function(){
         $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
       });  
-});  
+
+
+    //Owl
+    $('.hero-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        navText: ['PREV', 'NEXT'],
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        responsive: {
+            0: {
+                nav: false,
+            },
+            768: {
+                nav: true,
+            }
+        }
+    })
+});
